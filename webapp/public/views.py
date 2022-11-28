916989389cdb764747da560cd625ab8e8edf8e86
michaelwalkerfl <mailmichaelwalker@gmail.com>
2022-11-28 14:53:30 -0500

    add: public index page.

--- webapp/public/views.py
+++ webapp/public/views.py
@@ -1,4 +1,14 @@
 from flask import Blueprint
+from flask import render_template
 
-public = Blueprint('public', __name__)
+public = Blueprint(
+    'public',
+    __name__,
+    template_folder="templates",
+    static_folder='static',
+)
 
+
+@public.route('/')
+def index():
+    return render_template('index.jinja2')
